b22dd68ef795c70b2049ff3ff7a3b1c033efc0e8
Yiwei Gong <imwithye@gmail.com>
2023-09-25 13:43:15 +0800

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin init --apply imwithye
+cd $HOME/.local/share/chezmoi
+git remote set-url origin git@github.com:imwithye/dotfiles.git
+
