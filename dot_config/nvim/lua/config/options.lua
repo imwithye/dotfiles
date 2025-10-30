7734a6ed89f543099b109c332042566dc9d9cca8
Yiwei Gong <imwithye@gmail.com>
2025-10-30 23:48:29 +0800

    update option

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -4,6 +4,7 @@
 
 -- neovide
 vim.g.neovide_cursor_animation_length = 0.00
+vim.g.neovide_cursor_trail_size = 0
 vim.g.neovide_cursor_animate_in_insert_mode = false
 vim.g.neovide_cursor_animate_command_line = false
 
